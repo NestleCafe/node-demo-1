73c4f3d9c035d5b9aece1fe9d3e060b205938b08
NestleCafe <watson_xian@foxmail.com>
2021-03-08 01:54:18 +0800

    server.js commit

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`二哈`)
+    response.write(`<h1>标题</h1>`)
     response.end()
-  } else if(path === '/x'){
+  } else if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color: red;}`)
